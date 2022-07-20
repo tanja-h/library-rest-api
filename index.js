7efb87723333e7c91d41aa3fca85ef1bcf96d95f
nev13 <enastojadinovic9@gmail.com>
2022-07-20 23:27:56 +0200

    test

--- index.js
+++ index.js
@@ -26,3 +26,4 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
 
 //listen to server
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+//nevena
